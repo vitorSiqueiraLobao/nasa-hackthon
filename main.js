e2bc9806d28df87e3fb18d592f5bd1412d410718
vitorSiqueiraLobao <vitorsiqueiralobao@gmail.com>
2020-05-31 04:04:22 -0300

    final ?

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ polygonSeries.useGeodata = true;
 
 // Configure series
 var polygonTemplate = polygonSeries.mapPolygons.template;
-polygonTemplate.tooltipText = "{name}";
+polygonTemplate.tooltipText = "{name}"
 polygonTemplate.fill = am4core.color("yellow");
 
 
@@ -39,11 +39,12 @@ names.sort(function(a,b){
    return 0
 })
 
+let loadScreen = () =>{
 
-
-$.getJSON('dadoscertoscertos.json',function(data){
+$.getJSON('dados_agoracertos.json',function(data){
       data.forEach(function(data2){
-         nomes.push(data2.Country.Region)
+
+         nomes.push(data2.Region)
    })
 }).then((data)=>{
    nomes.sort(function(a,b){
@@ -51,14 +52,10 @@ $.getJSON('dadoscertoscertos.json',function(data){
       if(a>b)return 1
       return 0
    })
-   //console.log(nomes)
-
-   console.log(nomes)
+   
 
    nomes = [ ...new Set(nomes)]
-   //console.log(data)
-   console.log("nomes")
-   console.log(nomes)
+  
    desenhaCasos(nomes,data)
    
 })
@@ -81,20 +78,16 @@ let desenhaCasos = (nomes,data) =>{
          if(nome == chart.geodata.features[i].properties.name){
             idNovo = chart.geodata.features[i].properties.id
             nomeNovo = nome
-            if(nome == "China"){
-               console.log("AAAAAAAAAAAAAIDS")
-            }
          }
 
       }
       for(let i = 0; i <data.length;i++){
-         if(nome == data[i].Country.Region){
+         if(nome == data[i].Region){
             valor = data[i].Deaths
-            beg = 255 - Math.round(data[i].Deaths*0.0255)
-            if(beg < 0){
-               beg = 0
-            }
-            cor = 'rgb(255,'+beg+','+beg+')'
+            let stringValor = ""+valor
+           
+   
+            cor = returnColor(data[i],select)
          }
       }
       vetorPaises.push({
@@ -109,35 +102,84 @@ let desenhaCasos = (nomes,data) =>{
    
    polygonSeries.data = vetorPaises
    polygonTemplate.propertyFields.fill = 'fill';
-   console.log(cor)
-   vetorPaises.forEach((pinto)=>{
-      if(pinto.name == "China"){
-         console.log("MEU PINTAÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃÃO")
-         console.log(pinto)
-      }
-   })
-   console.log("data")
-   console.log(data)
-   console.log("vetorPaises")
-   console.log(vetorPaises)
-   console.log(nomes)
-   console.log(chart.geodata.features)
-   
-   
+
+
+
    
    //return vetorPaises
 }
 
+//100k - 255
+//1 - x
+
+let returnColor = (data) => {
+   let select = $("select").val()
+   if(select == "Deaths"){
+      beg = 255 - Math.round(data.Deaths*0.0255)
+      if(beg < 0){
+         beg = 0
+      }
+      return 'rgb(255,'+beg+','+beg+')'
+   }else if(select == 'Confirmed'){
+      beg = 255 - Math.round(data.Confirmed * 0.000255)
+      if(beg < 0){
+         beg = 0
+      }
+      return 'rgb(255,'+beg+',255)'
+   }else if(select == 'Recovered'){
+      beg = 255 - Math.round(data.Recovered * 0.00125)
+      if(beg < 0){
+         beg = 0
+      }
+      return 'rgb('+beg+',255,'+beg+')'
+   }else{
+      beg = 255 - Math.round(data.Sanitation * 2.55)
+      if(beg < 0){
+         beg = 0
+      }
+      let laranja = beg
+      if(laranja < 143){
+         laranja = 143
+      }
+      if(data.Sanitation == 0){
+         return 'yellow'
+      }else{ return 'rgb(255,'+laranja+','+beg+')'}
+   }
+}
+
+}
+$.getJSON('dadoscertoscertos.json').then(()=>{
+   loadScreen()
+})
 
 
 // Create hover state and set alternative fill color
 var hs = polygonTemplate.states.create("hover");
-hs.properties.fill = am4core.color("#367B25");
+
 
 var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
 graticuleSeries.mapLines.template.line.stroke = am4core.color("#67b7dc");
-graticuleSeries.mapLines.template.line.strokeOpacity = 0;
+graticuleSeries.mapLines.template.line.strokeOpacity = 0.2;
 graticuleSeries.fitExtent = false;
-
+hs.properties.fill = am4core.color("rgba(0,0,255,0.5)");
 chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#aadaff");
 chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 1;
+
+
+let update = () =>{
+   let select = $("select").val()
+   let tituloLegenda = document.querySelector("h1")
+   let dg = document.querySelector(".dg")
+   console.log(select == "Recovered")
+   if(select == 'Deaths'){
+      dg.style.background = 'linear-gradient(180deg, rgba(255,0,0,1) 0%, rgba(255,255,255,1) 100%)'
+   }else if(select == "Confirmed"){
+      dg.style.background = 'linear-gradient(180deg, rgba(255,0,255,1) 0%, rgba(255,255,255,1) 100%)'
+   }else if(select == "Recovered"){
+      dg.style.background = 'linear-gradient(180deg, rgba(0,255,0,1) 0%, rgba(255,255,255,1) 100%)'
+   }else{
+      dg.style.background = 'linear-gradient(180deg, rgba(255,143,0,1) 0%, rgba(255,255,255,1) 100%)'
+   }
+   tituloLegenda.innerHTML = select
+   loadScreen()
+}
---
+++ updateView.js
@@ -0,0 +1,2 @@
+let select = $("select").val()
+
